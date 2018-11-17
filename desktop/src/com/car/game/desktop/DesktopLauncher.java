3285e2756c9e6a08b2f9ec29a0713636a939ecb5
Nimerkuloff <Nimer38rus@gmail.com>
2018-11-18 01:29:53 +0700

    gradle downgrade to 2.14.1 Android plugin for Gradle is 2.1.3

---
+++ desktop/src/com/car/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.car.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.car.game.CarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CarGame(), config);
+	}
+}
